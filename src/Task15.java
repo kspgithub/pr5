5163b03c350a577c4eaaa5fffa1a868d33f205bb
KSP <semen1602@icloud.com>
2021-01-23 22:31:43 +0300

    Initial commit

---
+++ src/Task15.java
@@ -0,0 +1,12 @@
+public class Task15 {
+    public static String recursion(int n) {
+        if (n < 10) {
+            return Integer.toString(n);
+        } else {
+            return recursion(n / 10) + " " + n % 10;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(recursion(22121));
+    }
+}
